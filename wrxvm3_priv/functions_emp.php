e7ad7a1b21f88330c5873bdb8f1e12c4a92fae45
Emyll Almonte <eg0dz@aol.com>
2020-05-07 14:47:49 -0400

    updated priv functions

--- wrxvm3_priv/functions_emp.php
+++ wrxvm3_priv/functions_emp.php
@@ -1,13 +1,14 @@
 <?php
+//employee functions
 session_start();
 require_once('database.php');
 
 //supress mysql warnings for security purposes, but enable only after everything is working on the backend
-//ini_set( "display_errors", 0);
+ini_set( "display_errors", 0);
 
 
 //login button action
-if (isset($_POST['login_button'])) {
+if (isset($_POST['emp_login_button'])) {
 	$login_email = filter_input(INPUT_POST, 'login_email', FILTER_SANITIZE_EMAIL);
 	$login_password = filter_input(INPUT_POST, 'login_password');
 	$hashed_login_password = sha1($login_password);
@@ -33,25 +34,41 @@ function login() {
 	if (count($errors) == 0) {
 
 		// run the query on the customer table
-		$login_check = $db->query("SELECT * FROM almontee3_CUSTOMERS WHERE Email='$login_email' AND Password='$hashed_login_password'");
+		$login_check = $db->query("SELECT * FROM almontee3_EMPLOYEES WHERE Email='$login_email' AND Password='$hashed_login_password'");
 		$login_results = $login_check->fetchAll(PDO::FETCH_ASSOC);
 
 		if (count($login_results) == 1) { // if a user with entered email and password match is found
 
-			//return the array results
-			foreach ($login_results as $user){
-				if ($user['UserType'] == 'customer') {
+			//return the array of user
+			foreach ($login_results as $user) {
+
+				if ($user['UserType'] == 'sales') {
 					$_SESSION['user'] = $user;
 					$_SESSION['fname'] = $user['FirstName'];
-					$_SESSION['address'] = $user['Address'];
-					$_SESSION['vehiclemodel'] = $user['VehicleModel'];
-					$_SESSION['vehicleyear'] = $user['VehicleYear'];
-					$_SESSION['telephone'] = $user['Telephone'];
-					header("location: customer_dashboard.php");
+					$_SESSION['lname'] = $user['LastName'];
+					$_SESSION['employeeid'] = $user['EmployeeID'];
+					$_SESSION['jobtitle'] = $user['JobTitle'];
+					header("location: sales_dashboard.php");
+				}
+				if ($user['UserType'] == 'business') {
+					$_SESSION['user'] = $user;
+					$_SESSION['fname'] = $user['FirstName'];
+					$_SESSION['lname'] = $user['LastName'];
+					$_SESSION['employeeid'] = $user['EmployeeID'];
+					$_SESSION['jobtitle'] = $user['JobTitle'];
+					header("location: business_dashboard.php");
+				}
+				if ($user['UserType'] == 'dbadmin') {
+					$_SESSION['user'] = $user;
+					$_SESSION['fname'] = $user['FirstName'];
+					$_SESSION['lname'] = $user['LastName'];
+					$_SESSION['employeeid'] = $user['EmployeeID'];
+					$_SESSION['jobtitle'] = $user['JobTitle'];
+					header("location: dbadmin_dashboard.php");
 				}
 			}
-
-		} // if no user is found or password doesnt match
+		}
+		 // if no user is found or password doesnt match
 		else {
 			echo "<center><h4>(Error): User Not Found or Incorrect Password.</h4></center>";
 		}
@@ -62,7 +79,7 @@ function login() {
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
-	header("location: login.php");
+	header("location: login_emp.php");
 }
 
 ?>
